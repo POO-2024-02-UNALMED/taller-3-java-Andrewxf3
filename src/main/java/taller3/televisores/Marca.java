208c9be5774d8da031368280ef10373b10cd7ce3
Andrés Felipe Muñoz Ortiz <163070299+Andrewxf3@users.noreply.github.com>
2024-11-18 02:24:13 +0000

    1

---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,15 @@
+public class Marca{
+    public String nombre; 
+
+    public Marca(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
